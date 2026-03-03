d331409b0f91e91c1ce22bba0ae9f14c7dcb99f8
dfinkler <dfinkler@terminalsc.com.br>
2026-03-03 14:38:19 -0300

    rename

--- backend/DTOs/Auth/RegisterRequestDto.cs
+++ backend/DTOs/Auth/RegisterRequestDto.cs
@@ -7,6 +7,6 @@ public class RegisterRequestDto()
     public required string Email { get; set; }
     public required string Password { get; set; }
 
-     //userProfile
+    //userProfile
     public string? Bio { get; set;}
 }
